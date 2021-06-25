83d163e6b132701f1042690949f2b920eaee997c
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-06-25 22:41:28 +0600

    responsive done

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -13,19 +13,7 @@ const Banner = () => {
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a className="navbar-brand me-5"><h1>Bvlogger.com</h1></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="">Home</a>
-        </li>
-        <button onClick={gotoUploadBlog} type="button" class="btn btn-primary ">Login</button>
-
-
-      </ul>
-    </div>
+    <button onClick={gotoUploadBlog} type="button" class="btn btn-primary ">Login</button>
   </div>
 </nav>
         </div>
